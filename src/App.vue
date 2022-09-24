ee40a58e3ff48a04443494e2c7e34e966a6c5e79
东村的小香猪 <2124495707@qq.com>
2022-09-24 12:07:40 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    kaiyuan10nian
+  </div>
+
+</template>
+
+<style scoped lang="less">
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from "vue";
+// import "./style.css";
+import App from "./App.vue";
+// import "normalize.css/normalize.css";
+
+async () => {
+  //初始化UI
+  const uiApp = createApp(App);
+  //注册全局组件
+
+  //向根组件绑定全局变量
+
+  //
+
+  uiApp.mount("#app");
+};
